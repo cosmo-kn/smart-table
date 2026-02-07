0cd0c446e737ce655cabf17d1aa94a7e1b735969
Kamil Nuryasov <profilkamil404@gmail.com>
2026-02-07 21:28:34 +0300

    use API instead of local json fix3

--- src/components/searching.js
+++ src/components/searching.js
@@ -1,17 +1,4 @@
 export function initSearching(searchField) {
-  // @todo: #5.1 — настроить компаратор
-
-  // const compare = createComparison(
-  //   ['skipEmptyTargetValues'],
-  //   [
-  //     rules.searchMultipleFields(
-  //       searchField,
-  //       ["date", "customer", "seller"],
-  //       false
-  //     ),
-  //   ]
-  // );
-
   return (query, state, action) => {
     return state[searchField]
       ? Object.assign({}, query, {
